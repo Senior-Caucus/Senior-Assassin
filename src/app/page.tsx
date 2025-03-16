252460ceef3ec202d02a7f7e093aa4100cda792b
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-03-16 16:37:58 -0400

    add new files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
     <div className="">
-      <div className="text-7xl flex justify-center">
+      <div className="text-7xl flex justify-center mt-4">
         Senior Assassin Website
       </div>
+
+      
     </div>
   );
 }
