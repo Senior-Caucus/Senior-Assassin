5907ceffd0de78ae50ce292576099b2c68a11a66
Otzar Jaffe <otzarjaffe@gmail.com>
2025-05-26 15:19:43 +0000

    changes in dates

--- src/static/js/login.js
+++ src/static/js/login.js
@@ -89,7 +89,7 @@ document.getElementById("login")?.addEventListener("click", () => {
   const countdownEl = document.getElementById('countdown');
   function update() {
     // May 20, 2025 00:00 EST (UTC-4)
-    const target = new Date('2025-05-24T00:00:00-04:00');
+    const target = new Date('2025-05-26T00:00:00-04:00');
     const now = new Date();
     let diff = target - now;
     if (diff <= 0) {
@@ -100,7 +100,7 @@ document.getElementById("login")?.addEventListener("click", () => {
         loginButton.style.display = 'none';
       }
       // May 20, 2025 00:00 EST (UTC-4)
-      const target = new Date('2025-05-27T00:00:00-04:00');
+      const target = new Date('2025-05-28T00:00:00-04:00');
       const now = new Date();
       let diff = target - now;
       if (diff <= 0) {
